c87fed1d8652869689c8bf51831d0424c1153baa
Roman Turner <rn.turner@live.com>
2021-01-21 02:14:06 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+ final_results = data.each_with_object({}) do |(key, value), output|
+ #  binding.pry
+   value.each do |inner_key, names|
+     names.each do |name|
+       if !output[name] 
+       output[name] = {}
+  #      binding.pry
+       end
+       if !output[name][key]
+          !output[name][key] = []
+       end
+       output[name][key].push(inner_key.to_s)
+   end
+  end   
+end
+  #binding.pry
 end
